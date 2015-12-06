6ee62653bb25615af2881a1307b677c2c4a6f1c0
Todd Blose <todd@lana.io>
2015-12-05 16:37:31 -0800

    initial commit

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = '../README'
+master_doc = 'index.rst'
 
 # General information about the project.
 project = u'Lantern'
