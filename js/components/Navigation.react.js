68870bd83be4f6865a4e766e3f1190b4ec8b5fdf
Rogelio Alberto <i@rog.mx>
2016-01-07 14:43:00 +0000

    Setup Main Menu

--- js/components/Navigation.react.js
+++ js/components/Navigation.react.js
@@ -1,7 +1,6 @@
 /**
  *
  * Navigation.react.js
- *
  * This component is the navigation left bar, contain code that link to all the pages.
  */
 
@@ -11,16 +10,35 @@ import LeftNav from 'material-ui/lib/left-nav'
 import MenuItem from 'material-ui/lib/menus/menu-item'
 import RaisedButton from 'material-ui/lib/raised-button'
 
+import MenuItems from '../constants/MenuItems'
+
 export default class LeftNavSimpleExample extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = { open: false }
+    this.addMenuItem = this.addMenuItem.bind(this)
     this._handleToggle = this._handleToggle.bind(this)
   }
 
+  componentWillMount() {
+    const that = this
+    this.actions = {
+      'default': function defaultAction() {
+      },
+      'close': function closeMenu() {
+        that.setState({ open: false })
+      },
+    }
+  }
+
+  addMenuItem(item, i) {
+    return (
+      <MenuItem key={i} onTouchTap={this.actions[item.action]}>{item.title}</MenuItem>
+    )
+  }
   _handleToggle() {
-    this.setState({open: !this.state.open})
+    this.setState({ open: !this.state.open })
   }
 
   render() {
@@ -30,8 +48,7 @@ export default class LeftNavSimpleExample extends React.Component {
           label="Open Menu"
           onTouchTap={this._handleToggle} />
         <LeftNav open={this.state.open}>
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
+          {MenuItems.map(this.addMenuItem)}
         </LeftNav>
       </div>
     )
---
+++ js/constants/MenuItems.js
@@ -0,0 +1,36 @@
+const MenuItems = [
+  {
+    title: 'Share',
+    action: 'default',
+  },
+  {
+    title: 'Sign in to PRO',
+    action: 'default',
+  },
+  {
+    title: 'Language',
+    action: 'default',
+  },
+  {
+    title: 'Get Mobile Version',
+    action: 'default',
+  },
+  {
+    title: 'Contact',
+    action: 'default',
+  },
+  {
+    title: 'About',
+    action: 'default',
+  },
+  {
+    title: 'Settings',
+    action: 'default',
+  },
+  {
+    title: 'Exit',
+    action: 'close',
+  },
+]
+
+export default MenuItems
