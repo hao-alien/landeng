516bdcb94f43b9818dbb7cef80518a529be14a19
Rogelio Alberto <i@rog.mx>
2016-01-21 11:37:05 -0600

    About Dialog Style

--- js/components/dialogs/About.react.js
+++ js/components/dialogs/About.react.js
@@ -5,14 +5,17 @@ import LanternDialog from './Dialog.react'
 
 class About extends React.Component {
   render() {
-    return (<LanternDialog title="About"
-      icon = {<IconInfo color="white" />}>
-      <img src="/img/logo.svg" />
-      <h1>Open Internet for Everyone</h1>
-      <p>Lantern is an application that delivers fast, reliable and secure access to the open Internet.</p>
-      <a href="">Questions?</a>
-      <a href="">Read the Lantern FAQs</a>
-    </LanternDialog>)
+    return (
+      <LanternDialog title="About" icon = {<IconInfo color="white" />}>
+        <div id="about">
+          <img src="/img/logo.svg" />
+          <h1>Open Internet for Everyone</h1>
+          <p>Lantern is an application that delivers fast, reliable and secure access to the open Internet.</p>
+          <a href="">Questions?</a>
+          <a href="">Read the Lantern FAQs</a>
+        </div>
+      </LanternDialog>
+    )
   }
 }
 
