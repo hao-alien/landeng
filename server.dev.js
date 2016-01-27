bfce19d77ae8bd26a40ee495d0a120b5a6ed8d82
fffw <fffw@getlantern.org>
2016-01-27 14:47:58 +0800

    add mock ws backend

--- server.dev.js
+++ server.dev.js
@@ -3,15 +3,25 @@
 var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
 var config = require('./webpack.dev.config')
+var lanternBackendPort = 3031;
+var lanternBackend = 'http://localhost:' + lanternBackendPort;
+var backend = require('./mock-backend');
 
 console.log('Starting server...\n')
 
+backend(lanternBackendPort, console);
 new WebpackDevServer(webpack(config), { // Start a server
   publicPath: config.output.publicPath,
   hot: true, // With hot reloading
   inline: false,
   historyApiFallback: true,
-  quiet: false // Without logging
+  quiet: false, // Without logging
+  proxy: {
+    '/data': {
+      target: lanternBackend,
+      ws: true
+    }
+  }
 }).listen(2000, 'localhost', function (err, result) {
   if (err) {
     console.log(err)
