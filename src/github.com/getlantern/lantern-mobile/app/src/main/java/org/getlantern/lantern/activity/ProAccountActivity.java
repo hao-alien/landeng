98a1c3d6b97fbc60975a70c9e16110294f528510
Todd Blose <todd@lana.io>
2016-01-27 22:46:17 -0800

    add pro account management screen

---
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/activity/ProAccountActivity.java
@@ -0,0 +1,21 @@
+package org.getlantern.lantern.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.getlantern.lantern.R;
+ 
+
+public class ProAccountActivity extends Activity {
+
+    private static final String TAG = "ProAccountActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pro_account);
+    }
+}
+ 
