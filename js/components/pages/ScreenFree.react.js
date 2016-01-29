ed884aca0b5f72df27fe42dca598b8c742a59dbc
fffw <fffw@getlantern.org>
2016-01-29 19:04:08 +0800

    get referral code

--- js/components/pages/ScreenFree.react.js
+++ js/components/pages/ScreenFree.react.js
@@ -10,6 +10,9 @@ import RaisedButton from 'material-ui/lib/raised-button'
 import IconEmail from 'material-ui/lib/svg-icons/communication/email'
 
 import styles from '../../constants/Styles'
+import { asyncDialog } from '../../actions/AppActions'
+import { asyncCreateReferralCode } from '../../actions/ProAPIActions'
+import {PLANS_DIALOG} from '../../constants/Dialogs'
 
 class ScreenFree extends Component {
   constructor(props) {
@@ -22,16 +25,29 @@ class ScreenFree extends Component {
   }
 
   getCode() {
+    if (this._emailValidation()){
+      this.props.dispatch(asyncCreateReferralCode({
+        email: this.refs.email.getValue()
+      }))
+    }
+  }
 
+  upgrade() {
+    this.props.dispatch(asyncDialog({
+      open: true,
+      dialog: PLANS_DIALOG,
+    }))
   }
 
   _emailValidation() {
     const { t } = this.props
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const re = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (re.test( this.refs.email.getValue() )) {
       this.setState({ errorMail: '' })
+      return true
     } else {
       this.setState({ errorMail: t('free.use_valid_email') })
+      return false
     }
   }
 
@@ -39,7 +55,7 @@ class ScreenFree extends Component {
     const { t } = this.props
     return (
       <div>
-        <section id="middle_sheet">
+        <section id='middle_sheet'>
           <h2>{t('free.upgrade_headline')}</h2>
           <ul>
             <li>{t('free.li1')}</li>
@@ -47,21 +63,22 @@ class ScreenFree extends Component {
             <li>{t('free.li3')}</li>
             <li>{t('free.li4')}</li>
           </ul>
-          <div id="get_pro">
-            <RaisedButton label={t('free.upgrade_button')} />
+          <div id='get_pro'>
+            <RaisedButton label={t('free.upgrade_button')} onTouchTap={this.upgrade.bind(this)} />
           </div>
         </section>
-        <section id="bottom_sheet">
+        <section id='bottom_sheet'>
           <h3>{t('free.get_months_headline')}</h3>
           <p>{t('free.get_months_p')}</p>
-          <div id="get_code">
-            <IconEmail style={styles.iconStyles} color="white" />
+          <div id='get_code'>
+            <IconEmail id='icon_mail' style={styles.iconStyles} color='white' />
             <TextField
+              type='email'
               hintText={t('free.enter_email')}
               floatingLabelText={t('free.email')}
-              errorText={this.state.errorMail}
+              errorText={this.state.errorMail || t(this.props.data.error.message)}
               onBlur={this._emailValidation}
-              ref="email" />
+              ref='email' />
             <br />
             <RaisedButton label={t('free.get_code')} onTouchTap={this.getCode} />
           </div>
@@ -82,7 +99,7 @@ ScreenFree.propTypes = {
 // Which props do we want to inject, given the global state?
 function select(state) {
   return {
-    data: state,
+    data: state.pro,
   }
 }
 
--- js/reducers/proReducer.js
+++ js/reducers/proReducer.js
@@ -1,19 +1,25 @@
 import assignToEmpty from '../utils/assign'
-import {INITIATE_PURCHASE, PURCHASED, PURCHASE_FAILED} from '../constants/ProAPIConstants'
+import * as constants from '../constants/ProAPIConstants'
 
-const initialState = {}
+const initialState = {error: '', result: null}
 
 function proReducer(state = initialState, action) {
   Object.freeze(state) // Don't mutate state directly, always use assign()!
   switch (action.type) {
-  case INITIATE_PURCHASE:
-    return state
-  case PURCHASED:
-    return assignToEmpty({showResult: true, result: action.status})
-  case PURCHASE_FAILED:
-    return assignToEmpty({showError: true, error: action.status})
-  default:
-    return state
+    case constants.INITIATE_PURCHASE:
+    case constants.INITIATE_CREATE_USER:
+    case constants.INITIATE_CREATE_REFERRAL_CODE:
+      return assignToEmpty(state, initialState)
+    case constants.PURCHASED:
+    case constants.USER_CREATED:
+    case constants.REFERRAL_CODE_CREATED:
+      return assignToEmpty(state, {error: '', result: action.status})
+    case constants.PURCHASE_FAILED:
+    case constants.CREATE_USER_FAILED:
+    case constants.CREATE_REFERRAL_CODE_FAILED:
+      return assignToEmpty(state, {error: action.status, result: null})
+    default:
+      return state
   }
 }
 
