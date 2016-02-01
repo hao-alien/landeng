9d836797401a7b2ef8c793ccb5a2e5f9bd8fcd12
Todd Blose <todd@lana.io>
2016-01-31 16:11:00 -0800

    fix indentation

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/activity/PlansActivity.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/activity/PlansActivity.java
@@ -1,26 +1,43 @@
 package org.getlantern.lantern.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import android.net.Uri;
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
 
 import org.getlantern.lantern.activity.PaymentActivity;
+import org.getlantern.lantern.activity.CheckoutActivity;
 import org.getlantern.lantern.R;
 
 public class PlansActivity extends Activity {
 
     private static final String TAG = "PlansActivity";
+	private static final String mCheckoutUrl = "file:///android_asset/checkout.html";
+    private static final boolean useWebView = true;
 
     private Button getCodeBtn, monthBtn, yearBtn;
-
     private TextView featuresList;
 
     @Override
@@ -52,11 +69,65 @@ public class PlansActivity extends Activity {
 
     }
 
+	private class MyWebChromeClient extends WebChromeClient {
+		private Context mContext;
+
+        public MyWebChromeClient(Context context) {
+        	this.mContext = context;
+			super();
+		}
+		
+		@Override
+		public boolean onConsoleMessage (ConsoleMessage consoleMessage) {
+			Log.d(TAG, "Got a new console message: " + consoleMessage);
+			return true;
+		}
+
+		@Override
+		public boolean onJsAlert(WebView view, String url, String message, final android.webkit.JsResult result)  
+		{
+			Log.d("alert", message);
+			Toast.makeText(context, message, 3000).show();
+			result.confirm();
+			return true;
+		}; 
+	}
+
+
+	public void loadWebView() {
+
+		final Context mContext = this.getApplicationContext();
+
+        final WebView mWebView = new WebView();
+        mWebView.clearCache(true);
+
+		WebSettings mWebSettings = mWebView.getSettings();
+		mWebSettings.setJavaScriptEnabled(true);
+		mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+		mWebView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+       	mWebView.setWebChromeClient(new MyWebChromeClient(getApplicationContext()));
+
+		mWebView.loadUrl(mCheckoutUrl);
+
+		AlertDialog.Builder alert = new AlertDialog.Builder(this); 
+		alert.setView(mWebView);
+		alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.dismiss();
+			}
+		});
+		alert.show();
+	}
+
     public void selectPlan(View view) {
-        Log.d(TAG, "Plan selected...");
-        Intent intent = new Intent(this, PaymentActivity.class);
-        intent.putExtra("AMOUNT_TO_CHARGE", (String)view.getTag());
-        startActivity(intent);
+		Log.d(TAG, "Plan selected...");
+        if (useWebView) {
+            loadWebView();
+        } else {
+            intent.putExtra("AMOUNT_TO_CHARGE", (String)view.getTag());
+            startActivity(intent);
+        }
     }
 
 }  
