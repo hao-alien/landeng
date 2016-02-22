3b44181ccc104982548949e09b5a15f1e9e160c7
Todd Blose <todd@lana.io>
2016-02-22 01:03:25 -0800

    show 'Lantern PRO' title when user upgrades

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/activity/ProAccountActivity.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/activity/ProAccountActivity.java
@@ -1,6 +1,9 @@
 package org.getlantern.lantern.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +12,7 @@ import android.widget.ImageView;
 
 import android.support.v4.app.FragmentActivity;
 
+import org.getlantern.lantern.sdk.Utils;
 import org.getlantern.lantern.R;
  
 
@@ -20,6 +24,13 @@ public class ProAccountActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pro_account);
+    }
+
+    public void cancelPro(View view) {
+Context context = getApplicationContext();
+        SharedPreferences mPrefs = Utils.getSharedPrefs(context);
+        mPrefs.edit().remove("proUser").commit();
+        startActivity(new Intent(this, LanternMainActivity.class));
     }
 }
 
