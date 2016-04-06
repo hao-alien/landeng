8c52e0f089c9ea1c02958c4a1dd9c10430ad229c
Ulysses Aalto <uaalto@getlantern.org>
2016-04-06 12:11:37 -0700

    HTTP API for retrieving IPFS files

--- src/github.com/getlantern/ipfs-lantern/cmd/main.go
+++ src/github.com/getlantern/ipfs-lantern/cmd/main.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/getlantern/ipfs-lantern"
 	"github.com/mitchellh/go-homedir"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Please give a peer ID as an argument")
-		return
-	}
-
 	homedir, err := homedir.Dir()
 	if err != nil {
 		fmt.Println("Could not initialize IPFS: ", err)
 		return
 	}
 
-	err = ipfs.Run(os.Args[1], homedir+"/.ipfs")
+	ipfsSrv, err := ipfs.NewIPFSService(homedir + "/.ipfs")
 	if err != nil {
 		fmt.Println(err)
+		return
+	}
+
+	err = ipfsSrv.ServeHTTP()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 }
--- src/github.com/getlantern/ipfs-lantern/ipfs.go
+++ src/github.com/getlantern/ipfs-lantern/ipfs.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net/http"
 
 	logging "gx/ipfs/Qmazh5oNUVsDZTs2g59rq8aYQqwpss8tcUWQzor5sCCEuH/go-log"
 
@@ -15,40 +16,17 @@ import (
 	"golang.org/x/net/context"
 )
 
-func resolve(node *core.IpfsNode, ctx context.Context, name string) (string, error) {
-	p, err := node.Namesys.ResolveN(ctx, name, 1)
-	if err != nil {
-		return "", err
-	}
-
-	return p.String(), nil
+type IPFSService struct {
+	node    *core.IpfsNode
+	context *context.Context
 }
 
-func get(node *core.IpfsNode, ctx context.Context, pt string) (string, error) {
-	p := path.Path(pt)
-	dn, err := core.Resolve(ctx, node, p)
-	if err != nil {
-		return "", err
-	}
-
-	reader, err := uio.NewDagReader(ctx, dn, node.DAG)
-	if err != nil {
-		return "", err
-	}
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, reader)
-	if err != nil {
-		return "", err
-	}
-	return buf.String(), nil
-}
-
-func Run(target string, localRepo string) error {
+func NewIPFSService(localRepo string) (*IPFSService, error) {
 	logging.LevelInfo()
 
 	r, err := fsrepo.Open(localRepo)
 	if err != nil {
-		return fmt.Errorf("Error opening IPFS repo: %v", err)
+		return nil, fmt.Errorf("Error opening IPFS repo: %v", err)
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -61,22 +39,70 @@ func Run(target string, localRepo string) error {
 
 	nd, err := core.NewNode(ctx, cfg)
 	if err != nil {
-		return fmt.Errorf("Error initializing IPFS node: %v", err)
+		return nil, fmt.Errorf("Error initializing IPFS node: %v", err)
 	}
 
-	realPath, err := resolve(nd, ctx, target)
+	return &IPFSService{
+			node:    nd,
+			context: &ctx,
+		},
+		nil
+}
+
+func (srv *IPFSService) resolve(name string) (string, error) {
+	p, err := srv.node.Namesys.ResolveN(*srv.context, name, 1)
+	if err != nil {
+		return "", err
+	}
+
+	return p.String(), nil
+}
+
+func (srv *IPFSService) get(pt string) (string, error) {
+	p := path.Path(pt)
+	dn, err := core.Resolve(*srv.context, srv.node, p)
+	if err != nil {
+		return "", err
+	}
+
+	reader, err := uio.NewDagReader(*srv.context, dn, srv.node.DAG)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, reader)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func (srv *IPFSService) getIPNS(w http.ResponseWriter, r *http.Request) {
+	ref := r.URL.Query().Get("ref")
+	if ref == "" {
+		return
+	}
+
+	realPath, err := srv.resolve(ref)
 	if err != nil {
 		fmt.Printf("resolve: %s\n", err)
-		return fmt.Errorf("Error resolving IPNS link: %v", err)
+		return
+		//return fmt.Errorf("Error resolving IPNS link: %v", err)
 	}
-	fmt.Printf("Real path for %s: %s\n", target, realPath)
+	fmt.Printf("Real path for %s: %s\n", ref, realPath)
 
-	s, err := get(nd, ctx, realPath)
+	s, err := srv.get(realPath)
 	if err != nil {
 		fmt.Printf("get: %s\n", err)
-		return fmt.Errorf("Error retrieving IPFS file: %v", err)
+		return
+		//return fmt.Errorf("Error retrieving IPFS file: %v", err)
 	}
-	fmt.Println(s)
 
-	return nil
+	w.Write([]byte(s))
+}
+
+func (srv *IPFSService) ServeHTTP() error {
+	http.HandleFunc("/ipns-get", srv.getIPNS)
+
+	return http.ListenAndServe(":8799", nil)
 }
